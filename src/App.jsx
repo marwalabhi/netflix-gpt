ee76f90ea3c2219d7b940efddf6b9a5512e56663
marwalabhi <marwalabhi987@gmail.com>
2025-04-11 13:38:53 +0530

    vite setup & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="bg-sky-700 px-4 py-2 text-white text-shadow-amber-50 hover:bg-sky-800 sm:px-8 sm:py-3">
+        Netflix-GPT
+      </h1>
+    </>
+  );
+}
+
+export default App;
